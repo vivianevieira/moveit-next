5938461f884d5686d3046691689fd10cedc93ccb
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-24 11:58:08 -0800

    Added arrow icon to profile component.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile() {
       <img src="https://github.com/vivianevieira.png" alt="Viviane Vieira" />
       <div>
         <strong>Viviane Vieira</strong>
-        <p>Level 1</p>
+        <p>
+          <img src="icons/level.svg" alt="level" />
+          Level 1
+        </p>
       </div>
     </div>
   );
