4cd82fb12afa80232da77f8bb27dad6a53c0e762
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-24 12:44:22 -0800

    Added a title to the home page and a favicon.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,17 @@ import { ExperienceBar } from '../components/ExperienceBar';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 
+import Head from 'next/head';
+
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Home | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
