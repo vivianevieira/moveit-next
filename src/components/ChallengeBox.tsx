d9e286004fd2c31ca29ff19e807bb3a67dd4cce8
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-25 13:25:07 -0800

    Styled container for a notActive status and created a conditional to show correct challenge box.

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,15 +1,21 @@
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
   return(
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeNotActive}>
-        <strong>You must finish a cycle in order to start a new challenge</strong>
-      </div>
-      <p>
-        <img src="icons/level-up.svg" alt="Level up" />
-        Advance to the next level by completing a challenge.
-      </p>
+      { hasActiveChallenge ? (
+        <div />
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>You must finish a cycle in order to start a new challenge</strong>
+          <p>
+          <img src="icons/level-up.svg" alt="Level up" />
+          Advance to the next level by completing a challenge.
+          </p>
+        </div>
+      )}
     </div>
   )
 }
