5f71eee7fc19230c7c58e3340092b420c47c5a71
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-25 13:51:33 -0800

    Completed challenge box component and its styles.

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -6,7 +6,30 @@ export function ChallengeBox() {
   return(
     <div className={styles.challengeBoxContainer}>
       { hasActiveChallenge ? (
-        <div />
+        <div className={styles.challengeActive}>
+          <header>Win 400 xp</header>
+
+          <main>
+            <img src="icons/body.svg" />
+            <strong>New Challenge</strong>
+            <p>Get up and get moving!</p>
+          </main>
+
+          <footer>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+            >
+              Failed
+            </button>
+            <button
+              type="button"
+              className={styles.challengeSucceededButton}
+            >
+              Completed
+            </button>
+          </footer>
+        </div>
       ) : (
         <div className={styles.challengeNotActive}>
           <strong>You must finish a cycle in order to start a new challenge</strong>
