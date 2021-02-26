81a353002d27d88e9f7c83af2390903f5c670307
Viviane Vieira <viviane.kodama@gmail.com>
2021-02-26 15:09:24 -0800

    Created a function in Context that is triggered once Completed challenge button is clicked. Function updateds the level on the experience bar.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/vivianevieira.png" alt="Viviane Vieira" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Viviane Vieira</strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
